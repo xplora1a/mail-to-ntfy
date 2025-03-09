d70d557c6bbd0e91a6a50f2317cccf602222c65b
Stuart Ward <stuart.ward@bcs.org>
2025-03-09 18:19:46 +0000

    basic corrections

--- mail-to-ntfy.php
+++ mail-to-ntfy.php
@@ -8,12 +8,21 @@ Version: 1.0
 Author: Stuart Ward
 */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'MAIL_TO_NTFY_VERSION', '1.0.0' );
+
+
+
 function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
     $atts = apply_filters( 'wp_mail', compact( 'to', 'subject', 'message', 'headers', 'attachments' ) );
 
-    // sena notification via ntfy using curl
+    // send a notification via ntfy using curl
     $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,'https://nfty.sh/readingcyclecampaigne');
+    curl_setopt($ch,CURLOPT_URL,'https://ntfy.sh/readingcyclecampaigne');
     curl_setopt($ch,CURLOPT_POST,1);
     curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query(array('to'=>$atts['to'],'subject'=>$atts['subject'],'message'=>$atts['message'])));
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
