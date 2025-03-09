da0112bbe7b31b57c310fb48bb093e7c7e4efa3a
Stuart Ward <stuart.ward@bcs.org>
2025-03-09 19:33:22 +0000

    Now uses a filter to hook the wp_mail sending function

--- mail-to-ntfy.php
+++ mail-to-ntfy.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Mail to Ntfy
 Plugin URI: https://github.com/xplora1a/mail-to-ntfy
-Description: Will replace the default wp_mail function with a custom function that sends the email via ntfy.sh
+Description: Will send a ntfy message as well as sending the email;
 Version: 1.0
 Author: Stuart Ward
 */
@@ -15,17 +15,21 @@ if ( ! defined( 'WPINC' ) ) {
 
 define( 'MAIL_TO_NTFY_VERSION', '1.0.0' );
 
-function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
-    $atts = apply_filters( 'wp_mail', compact( 'to', 'subject', 'message', 'headers', 'attachments' ) );
+add_filter('wp_mail','ntfy_mails', 10,1);
+
+function ntfy_mails($args){
+    $to = $args['to'];
+    $subject = $args['subject'];
+    $message = $args['message'];
 
     // send a notification via ntfy using curl
     $ch = curl_init();
     curl_setopt($ch,CURLOPT_URL,'https://ntfy.sh/readingcyclecampaigne');
     curl_setopt($ch,CURLOPT_POST,1);
-    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query(array('to'=>$atts['to'],'subject'=>$atts['subject'],'message'=>$atts['message'])));
+    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query('to:'+$to+' subject:'+$subject+' message:'+$message));
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
     $result = curl_exec($ch);
     curl_close($ch);     
 
-    return true;
+    return $args;
 }
